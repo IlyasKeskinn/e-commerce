94577a3782fbebb3bbf645068ad0b999409036a5
İlyas Keskin <ilyaskeskn8@gmail.com>
2024-04-21 22:16:52 +0300

    Implement routing using React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,30 @@
 import { useState } from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { MainLayout } from './layout/MainLayout'
-
+import { Home } from './pages/Home/Home'
+import { Shop } from './pages/Shop/Shop'
+import { ShopingCart } from './pages/ShopingCart/ShopingCart'
+import { ShopCheckout } from './pages/ShopCheckout/ShopCheckout'
+import { ShopComplete } from './pages/ShopComplete/ShopComplete'
+import { Auth } from "./pages/Auth/Auth"
 
 function App() {
-  return (
-    <>
-    <MainLayout/>
-    </>
-  )
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/index", element: <Home /> },
+        { path: "/shop", element: <Shop /> },
+        { path: "/cart", element: <ShopingCart /> },
+        { path: "/checkout", element: <ShopCheckout /> },
+        { path: "/complete", element: <ShopComplete /> },
+        { path: "/login_register", element: <Auth /> },
+      ]
+    }
+  ])
+  return (<RouterProvider router={router}></RouterProvider>)
 }
 
 export default App
--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -1,20 +1,14 @@
 import { Header } from "../components/Layout/Header/Header";
 import { Footer } from "../components/Layout/Footer/Footer";
 import {AsideLayout} from "./AsideLayout"
-import { Home } from "../pages/Home/Home";
-import { Shop } from "../pages/Shop/Shop";
-import { ShopingCart } from "../pages/ShopingCart/ShopingCart";
-import { ShopCheckout } from "../pages/ShopCheckout/ShopCheckout";
-import { ShopComplete } from "../pages/ShopComplete/ShopComplete";
-import { NotFound } from "../pages/NotFoundPage/NotFound";
-import { Auth } from "../pages/Auth/Auth";
+import { Outlet } from "react-router-dom";
 
 export const MainLayout = () => {
     return (
         <>
             <Header />
             <main>
-                <Auth/>
+                <Outlet/>
             </main>
             <AsideLayout/>
             <Footer />
