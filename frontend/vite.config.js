5e96be2729c60ba91b5e45ee548ff61ff0504fe3
İlyas Keskin <ilyaskeskn8@gmail.com>
2024-05-09 17:39:34 +0300

    config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base : "/zephyra/",
+  base : "/e-commerce/",
 
   build: {
     chunkSizeWarningLimit: 5000,
