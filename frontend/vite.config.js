a27b16492a6386dc5ea8d62625a4ebbb2be6170f
İlyas Keskin <ilyaskeskn8@gmail.com>
2024-05-09 04:47:03 +0300

    home

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,19 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/zephyra/",
   plugins: [react()],
 
   build: {
-    chunkSizeWarningLimit: 8000,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
-      }
-    }
+    chunkSizeWarningLimit: 18000,
+
   }
 })
